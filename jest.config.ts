01a51d8f99696a331a3771433d8180fb59ab7678
Enzo Santos <enzo.santos29@hotmail.com>
2026-05-07 12:31:47 -0300

    chore: fix jest.config.ts and update test script

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "jest"
+
+const config: Config = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+
+    extensionsToTreatAsEsm: [".ts"],
+
+    transform: {
+        "^.+\\.ts$": [
+            "ts-jest",
+            {
+                useESM: true,
+            },
+        ],
+    },
+
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+}
+
+export default config
