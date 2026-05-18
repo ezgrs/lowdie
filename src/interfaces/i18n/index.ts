5903f72307bfc0215463ed250d0951579ec57108
Enzo Santos <enzo.santos29@hotmail.com>
2026-05-18 04:04:06 -0300

    fix: update i18n imports to use 'with { type: 'json' }'

--- src/interfaces/i18n/index.ts
+++ src/interfaces/i18n/index.ts
@@ -1,13 +1,13 @@
 import i18n from "i18next"
-import enBot from "./locales/en/bot.json"
-import enCommon from "./locales/en/common.json"
-import enRps from "./locales/en/rps.json"
-import enTtt from "./locales/en/ttt.json"
+import enBot from "./locales/en/bot.json" with { type: 'json' }
+import enCommon from "./locales/en/common.json" with { type: 'json' }
+import enRps from "./locales/en/rps.json" with { type: 'json' }
+import enTtt from "./locales/en/ttt.json" with { type: 'json' }
 
-import ptBot from "./locales/pt/bot.json"
-import ptCommon from "./locales/pt/common.json"
-import ptRps from "./locales/pt/rps.json"
-import ptTtt from "./locales/pt/ttt.json"
+import ptBot from "./locales/pt/bot.json" with { type: 'json' }
+import ptCommon from "./locales/pt/common.json" with { type: 'json' }
+import ptRps from "./locales/pt/rps.json" with { type: 'json' }
+import ptTtt from "./locales/pt/ttt.json" with { type: 'json' }
 
 i18n.init({
     lng: "pt",
